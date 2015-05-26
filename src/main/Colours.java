22bd89c9b6d17e7ae385179e6a7612b13469113f
Jake Stanley <madstanners@icloud.com>
2015-05-26 02:06:38 +0100

    Added GUI colours, updated constants

--- src/main/Colours.java
+++ src/main/Colours.java
@@ -9,6 +9,8 @@ public class Colours {
 
     public static final Color GRID_BACKGROUND = new Color(0, 0, 139); // NAVY BLUE TODO put these in a values class
     public static final Color GRID_LINES = new Color(65, 105, 225); // LIGHTER BLUE
+    public static final Color GUI_BACKGROUND = Color.black;
+    public static final Color GUI_BORDER = Color.yellow;
 
     public static final class Tiles {
 
--- src/main/Display.java
+++ src/main/Display.java
@@ -8,13 +8,20 @@ public class Display {
     public static int SCALE = 4;
     public static int FRAME_RATE = 30;
 
-    // DISPLAY CONSTANTS
+    // DISPLAY CONSTANTS // TODO CONSIDER a column based system?
     public static final int DISPLAY_WIDTH = 256;
-    public static final int DISPLAY_HEIGHT = 152;
+    public static final int DISPLAY_HEIGHT = 144;
 
-    // GAME CONSTANTS
+    // INTERFACE CONSTANTS
+    public static final int CONTROL_HINTS_BOX_HEIGHT = 8;
+    public static final int CONTROL_HINTS_BOX_WIDTH = 160;
+
+    public static final int MESSAGE_BOX_HEIGHT = 40; // SHOULD GIVE ROOM FOR FIVE MESSAGES
+    public static final int MESSAGE_BOX_WIDTH = 96; // TODO make these more responsive, possibly by using that column system?
+
+    // MAP CONSTANTS
     public static final int MAP_WIDTH = 160;
-    public static final int MAP_HEIGHT = 144;
+    public static final int MAP_HEIGHT = 136;
     public static final int TILE_WIDTH = 8;
     public static final int DOOR_WIDTH = 4;
     public static final int MOB_WIDTH = 2; // TODO make this dynamic
