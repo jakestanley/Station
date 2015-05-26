f4241e51a0e522c756ce0e774c3f740bb28edad6
Jake Stanley <madstanners@icloud.com>
2015-05-26 04:57:19 +0100

    Added/modified GUI colours

--- src/main/Colours.java
+++ src/main/Colours.java
@@ -7,10 +7,13 @@ import org.newdawn.slick.Color;
  */
 public class Colours {
 
+    public static final Color HOVER_SELECT = Color.white; // TODO better colours. gradients even?
     public static final Color GRID_BACKGROUND = new Color(0, 0, 139); // NAVY BLUE TODO put these in a values class
     public static final Color GRID_LINES = new Color(65, 105, 225); // LIGHTER BLUE
     public static final Color GUI_BACKGROUND = Color.black;
+    public static final Color GUI_BACKGROUND_SELECTED = Color.lightGray;
     public static final Color GUI_BORDER = Color.yellow;
+    public static final Color GUI_TEXT = Color.white;
 
     public static final class Tiles {
 
