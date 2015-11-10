394b2cb1824ac009433345377860ffb9ef11720d
Jake Stanley <prudentia713@gmail.com>
2015-11-10 22:44:53 +0000

    Added a 3D renderer class (needs work)

---
+++ src/uk/co/jakestanley/commander/rendering/world/ThreeDimensionalRenderer.java
@@ -0,0 +1,26 @@
+package uk.co.jakestanley.commander.rendering.world;
+
+import org.newdawn.slick.Graphics;
+import uk.co.jakestanley.commander.rendering.Renderer;
+
+/**
+ * Created by jp-st on 10/11/2015.
+ */
+public class ThreeDimensionalRenderer extends Renderer {
+
+    public ThreeDimensionalRenderer(int x, int y, int width, int height){
+        super(x, y, width, height);
+    }
+
+    public void init() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void render(Graphics screen) {
+
+    }
+}
