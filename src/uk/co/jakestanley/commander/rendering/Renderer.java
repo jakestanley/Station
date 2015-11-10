faac299eb0194cd5df7639b9eb85c0cd695301a7
Jake Stanley <prudentia713@gmail.com>
2015-11-10 22:45:21 +0000

    Renderer superclass now has the canvas variables

--- src/uk/co/jakestanley/commander/rendering/Renderer.java
+++ src/uk/co/jakestanley/commander/rendering/Renderer.java
@@ -1,14 +1,26 @@
 package uk.co.jakestanley.commander.rendering;
 
+import lombok.Getter;
 import uk.co.jakestanley.commander.interfaces.Loopable;
 import uk.co.jakestanley.commander.interfaces.Renderable;
 
 /**
  * Created by jp-st on 08/11/2015.
  */
+@Getter
 public abstract class Renderer implements Loopable, Renderable {
 
-    public Renderer(){
+    // canvas values; drawable space
+    protected int x;
+    protected int y;
+    protected int width;
+    protected int height;
+
+    public Renderer(int x, int y, int width, int height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
         // TODO something
     }
 
--- src/uk/co/jakestanley/commander/rendering/world/WorldRenderer.java
+++ src/uk/co/jakestanley/commander/rendering/world/WorldRenderer.java
@@ -9,18 +9,8 @@ import uk.co.jakestanley.commander.rendering.Renderer;
 @Getter
 public abstract class WorldRenderer extends Renderer {
 
-    // canvas values; drawable space
-    protected int x;
-    protected int y;
-    protected int width;
-    protected int height;
-
     public WorldRenderer(int x, int y, int width, int height){
-        super();
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+        super(x, y, width, height);
     }
 
 }
