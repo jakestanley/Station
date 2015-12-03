af8dd88413553057796750c279c161fda7d9ffb4
Jake Stanley <prudentia713@gmail.com>
2015-12-03 22:42:01 +0000

    changed background colour

--- src/uk/co/jakestanley/commander/rendering/world/threedimensional/ThreeDimensionalRenderer.java
+++ src/uk/co/jakestanley/commander/rendering/world/threedimensional/ThreeDimensionalRenderer.java
@@ -49,7 +49,7 @@ public class ThreeDimensionalRenderer { // TODO better inheritance
         GL11.glEnable(GL11.GL_DEPTH_TEST); // tests which triangles are on top and renders them in the correct order
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT); // clear colour for next frame
         GL11.glClear(GL11.GL_DEPTH_BUFFER_BIT); // clear depth buffer for next frame
-        GL11.glClearColor(0, 0, 0, 1); // TODO set a proper background colour
+        GL11.glClearColor(200, 50, 50, 1); // TODO set a proper background colour
     }
 
     public void render(RenderEntity entity, StaticShader shader) { // TODO need models/shapes/objects list or something
