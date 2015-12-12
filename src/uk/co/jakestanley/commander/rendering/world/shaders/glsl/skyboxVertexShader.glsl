01b8f87ff74f6a186d4642b255b2194c1fe5282f
Jake Stanley <prudentia713@gmail.com>
2015-12-12 16:58:44 +0000

    added skybox shaders

---
+++ src/uk/co/jakestanley/commander/rendering/world/shaders/glsl/skyboxVertexShader.glsl
@@ -0,0 +1,16 @@
+#version 400 core
+
+// from ThinMatrix tutorials. TODO credit accordingly
+
+in vec3 position;
+out vec3 textureCoords;
+
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;
+
+void main(void){
+
+	gl_Position = projectionMatrix * viewMatrix * vec4(position, 1.0);
+	textureCoords = position;
+
+}
